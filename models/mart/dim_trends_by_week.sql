db596eb7228353fc20e378c4ef8991dcde027b07
Matt Skogstad-Stubbs <mskogstadstubbs@gmail.com>
2023-05-05 18:19:35 +0000

    attempts to get the sql right

--- models/mart/dim_trends_by_week.sql
+++ models/mart/dim_trends_by_week.sql
@@ -2,39 +2,29 @@ with international_top_terms as (
     SELECT 
         term,
         week,
-        score,
         rank,
         country_name
 
     FROM {{ ref('stg_intl_top_terms') }}
+    GROUP BY week, rank, term, country_name
 ), 
 
 usa_top_terms as (
 
     SELECT 
-        score as usa_score,
         rank as usa_rank,
         term as usa_term,
-        week
+        distinct week
     FROM {{ ref('stg_top_terms') }}
+    GROUP BY week, rank, term
 ),
 
 joined_top_terms as (
-    SELECT * 
+    SELECT *
     FROM international_top_terms 
     JOIN usa_top_terms using (week)
-),
-
-final as (
-    SELECT 
-        *
-    FROM 
-        joined_top_terms
-    GROUP BY 
-        week
-    ORDER BY 
-        week, rank 
     LIMIT 100
 )
 
-SELECT * FROM final
+select DISTINCT * from joined_top_terms
+ORDER BY week, rank, usa_rank
